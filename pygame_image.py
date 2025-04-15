f5d7dee51446b74554bde91e77a17bbb6f24e0fa
nazca <c0a2428625@edu.teu.ac.jp>
2025-04-15 15:33:21 +0900

    change

---
+++ pygame_image.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import pygame as pg
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+
+def main():
+    pg.display.set_caption("はばたけ！こうかとん")
+    screen = pg.display.set_mode((800, 600))
+    clock  = pg.time.Clock()
+    bg_img = pg.image.load("fig/pg_bg.jpg")
+    bg_img2 = pg.image.load("fig/pg_bg.jpg")
+    bg_img_flip = pg.transform.flip(bg_img, True, False)
+    koukaton = pg.image.load("fig/3.png")
+    koukaton_flip = pg.transform.flip(koukaton, True, False)
+    tmr = 0
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT: return
+
+        screen.blit(bg_img, [0, 0])
+        screen.blit(koukaton_flip, [300, 200])
+        pg.display.update()
+        tmr += 1        
+        clock.tick(200)
+
+
+if __name__ == "__main__":
+    pg.init()
+    main()
+    pg.quit()
+    sys.exit()
